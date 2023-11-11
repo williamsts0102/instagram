e6b1fe54b0acba748bb6ea733d06435acb12a885
williamsts0102 <developer.williamstoro@gmail.com>
2023-11-11 18:54:06 -0500

    creacion

---
+++ instagram/instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by DAMII on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
